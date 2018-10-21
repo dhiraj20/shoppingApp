16d9866c883de53c1a68ab9606dd789e05eceb57
Dhiraj Majhi <dhirajmajhi@Dhirajs-MacBook-Air.local>
2018-10-21 16:36:33 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+
+import { AppInterceptor } from './theme/utils/app-interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSpinnerModule,
+    HttpClientModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AppInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
